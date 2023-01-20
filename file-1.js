f7b472b8a1e49129a8d710364fa9f05621ab2db9
GurIlVik <gilvik1@yandex.ru>
2023-01-20 16:30:22 +0300

    удаление из списка городов компа озвученные юзером

--- file-1.js
+++ file-1.js
@@ -12,7 +12,9 @@ let sity_comp = '';
 let list_sity_post = [];
 
 function get_next_city(my_city) {
+   
 	for (var i = 0; i < sitys.length; i++) {
+        
 		if (sitys[i][0].toLowerCase() === my_city[my_city.length - 1]) {
 			let result = sitys[i];
             list_sity_post.push(result);
@@ -21,6 +23,10 @@ function get_next_city(my_city) {
 			return result;
 		}
 	}
+    for (let i = 0; i < sitys.length; i++) {
+        list_sity_post.push(sitys[i]);
+    }
+    console.log(sitys);
 	return "Ты выиграл!"
 }
 
@@ -29,6 +35,8 @@ function writer (sity_comp, sity_user) {
     document.getElementById('s3').innerHTML = sity_comp[sity_comp.length -1];
     document.getElementById('s2').innerHTML += '<br>' + sity_user + " - " +  sity_comp;
     list_sity_post.push(sity_user);
+    sitys = sitys.filter(item => item !== sity_user);
+    document.getElementById('a1').innerHTML = ''; // почему не работает innerHTML 
 }
 
 function checkuser (use) {
@@ -37,6 +45,11 @@ function checkuser (use) {
         console.log(use[0])
         if (use[0] === letters[i]) {
             console.log(use[0])
+            for (let j = 0; j < list_sity_post.length; j ++) {
+                if (list_sity_post[j] == use){
+                    return 'Этот город уже был, введите другой.'
+                }
+            }
             return use
         } 
        };  return false}
@@ -47,10 +60,14 @@ function start (sitys) {
             // console.log(sity_comp);
         var sity_user = document.getElementById('a1').value;
         sity_user = checkuser(sity_user);
+        // sity_user = checkuser(sity_user);
         if (sity_user === false) {
             document.getElementById('s2').innerHTML += '<br>' + 'Введите правильно город, первая буква указана не верно:';
             document.getElementById('a1').innerHTML = '';
         } else {
+            if (sity_user == 'Этот город уже был, введите другой.') {
+                document.getElementById('s2').innerHTML += '<br>' + sity_user
+            } else {
             if (sity_comp === '') {
                 sity_comp = get_next_city(sity_user)
                 writer (sity_comp, sity_user)
@@ -60,7 +77,7 @@ function start (sitys) {
             } else {
                 sity_comp = get_next_city(sity_user)
                 writer (sity_comp, sity_user)
-        }}};}
+        }}}};}
         }
     
 window.onload = () => {
